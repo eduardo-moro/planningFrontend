472a2e7eff3e66a3a3ec824a63737d26dd8193fa
Eduardo Henrique de Paula Moro <46352760+eduardo-moro@users.noreply.github.com>
2023-04-05 14:05:41 -0300

    updating info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <v-flex mt-8 mb-10>
     <h1>Olá!</h1>
-    <h2>Se você está lendo está página significa que entreguei com êxito o desafio técnico da UPlanner! </h2>
+    <h2>Programa de planejamento financeiro</h2>
   </v-flex>
 </template>
 
